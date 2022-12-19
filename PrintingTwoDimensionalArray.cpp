181d15802697cae3bad6da5edea2f9ac863a3c52
Sonam Kumari <106435692+sonamyadav6942@users.noreply.github.com>
2022-12-19 20:30:18 +0530

    Create PrintingTwoDimensionalArray.cpp

---
+++ PrintingTwoDimensionalArray.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int A[3][3] = {{2,4,6},{3,6,9},{5,3,7}};
+	for(int i = 0; i < 3; i++)
+	{
+		for(int j = 0; j < 3; j++)
+		{
+			cout << A[i][j];
+		}
+		cout << endl;
+	}
+	return 0;
+}
