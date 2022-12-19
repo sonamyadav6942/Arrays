2c2c9719c133a34f3cdfdbe53f803a124e49f2fd
Sonam Kumari <106435692+sonamyadav6942@users.noreply.github.com>
2022-12-19 14:32:22 +0530

    Update FIndingMaxElementFromAnArray.cpp

--- FIndingMaxElementFromAnArray.cpp
+++ FIndingMaxElementFromAnArray.cpp
@@ -3,10 +3,9 @@ using namespace std;
     
 int main()
 {
-	int A[7]={4,8,6,9,5,2,7};
 	int n=7,max;
+	int A[7]={4,8,6,9,5,2,7};
 	max=A[0];
-	
 	for(int i=0;i<7;i++)
 	{
 		if(A[i]>max)
@@ -14,7 +13,7 @@ int main()
 		    max=A[i];
 		}
 	}	
-	cout<<"Maxmium no is : "<<max;
+	cout<<"Maxmium element from an array is : "<<max;
 	
 	return 0;
 	
