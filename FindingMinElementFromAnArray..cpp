1b04dd48be0c7f721d8f55caf9b8b6d901e3caae
Sonam Kumari <106435692+sonamyadav6942@users.noreply.github.com>
2023-03-28 19:54:28 +0530

    Update FindingMinElementFromAnArray..cpp

--- FindingMinElementFromAnArray..cpp
+++ FindingMinElementFromAnArray..cpp
@@ -18,3 +18,15 @@ int main()
 	return 0;
 	
 }
+
+
+or
+	
+int min;
+int arr[5] = {9,5,3,7,1};
+for(int i = 0; i < 5; i++)
+{
+    if(arr[i] < arr[0])
+    min = arr[i];  
+}
+cout << "Minimum element from an array are : " << min << endl;
