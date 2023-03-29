e1861b2277a41a021df1d7888ef4ec748164b8aa
Sonam Kumari <106435692+sonamyadav6942@users.noreply.github.com>
2023-03-29 17:22:26 +0530

    Update CountEvenAndOddElements.cpp

--- CountEvenAndOddElements.cpp
+++ CountEvenAndOddElements.cpp
@@ -19,3 +19,33 @@ int main()
 
 
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+  
+  
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[100];
+    int num;
+    int even_count = 0, odd_count = 0;
+    cout << "Enter the size of element in an array : ";
+    cin >> num;
+    cout << "Number of elements in an array are : ";
+    for(int i = 0; i < num; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < num; i++)
+    {
+        if(arr[i] % 2 == 0)
+        even_count++;
+        else
+        odd_count++;
+    }
+    cout << "Even Elements count : " << even_count << "\nOdd Elements count : " << odd_count;
+    return 0;
+}
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------------
