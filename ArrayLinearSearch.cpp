c447a67332632afbcabcc7c8284f815fe5e0ddb5
Sonam Kumari <106435692+sonamyadav6942@users.noreply.github.com>
2023-03-31 11:29:45 +0530

    Update ArrayLinearSearch.cpp

--- ArrayLinearSearch.cpp
+++ ArrayLinearSearch.cpp
@@ -1,30 +1,29 @@
-#include<iostream>
-using namespace std;
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+#include <iostream>
+using namespace std;
 int main()
 {
- 	int A[10],n=10;
-	int key;
-	cout<<"Enter an element of array : ";
-	    
-	for(int i=0;i<n;i++)
-	{
-		cin>>A[i];
-	}
-	cout<<"Enter an element to search : ";
-	cin>>key;
-	    
-	for(int i=0;i<n;i++)
-	{
-		if(key==A[i])
-		{
-			cout<<"Found at index number : "<< i;
-			return 0;
-		}
-	}
-	    
-	cout<<"Not Found";
-	
-	return 0;
-	    
-}		
+    int arr[100], num, key;
+    cout << "Enter the size of an array : ";
+    cin >> num;
+    cout << "Enter an array element : ";
+    for(int i = 0; i < num; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Enter an element to search : ";
+    cin >> key;
+    for(int i = 0; i < num; i++)
+    {
+        if(arr[i] == key)
+        {
+            cout << "Element found at index number : " << i;
+            return 0;
+        }        
+    }
+    cout << "Not Found";
+    return 0;
+}
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
