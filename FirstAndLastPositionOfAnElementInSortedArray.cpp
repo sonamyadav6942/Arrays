55ba15497e03c4644e1c75e5d95f00c0ca5bc231
Sonam Kumari <106435692+sonamyadav6942@users.noreply.github.com>
2023-04-05 13:00:10 +0530

    Create FirstAndLastPositionOfAnElementInSortedArray.cpp

---
+++ FirstAndLastPositionOfAnElementInSortedArray.cpp
@@ -0,0 +1,62 @@
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
+ #include <iostream>
+using namespace std;
+int firstOcc(int arr[], int size, int key)
+{
+    int start = 0, end = size - 1;
+    int mid = start + (end - start) / 2;
+    int ans = -1;
+while(start <= end){
+    if(arr[mid] == key){
+    ans = mid;
+    end = mid - 1;
+    }
+        else if(key > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else if(key < arr[mid])
+        {
+            end = mid - 1;
+        }
+        mid = start + (end - start) / 2;
+}
+    return ans;
+}
+int lastOcc(int arr[], int size, int key){
+    int start = 0, end = size - 1;
+    int mid = start + (end - start) / 2;
+    int ans = -1;
+while(start <= end)
+{
+    if(arr[mid] == key)
+    {
+    ans = mid;
+    start = mid + 1;
+}
+      else if(key > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else if(key < arr[mid])
+        {
+            end = mid - 1;
+        }
+    
+    mid = start + (end - start) / 2;
+}
+    return ans;
+}
+
+int main()
+{
+    int even[5] = {1, 2, 3, 3, 5};
+
+    cout << "First Occurence of 3 at index : " << firstOcc(even, 5, 3) << endl;
+    cout << "Last Occurence of 3 at index : " << lastOcc(even, 5, 3) << endl;
+
+}
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------
